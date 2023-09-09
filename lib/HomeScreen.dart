7d8332b7b3febf73a8829f3460dad9e5f0bc79a5
Gopala05 <120028677+Gopala05@users.noreply.github.com>
2023-09-10 01:56:31 +0530

    WearOS

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
-import 'package:slide_to_act/slide_to_act.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
