e12a01dac65507b125c66cdad5f6e65060460835
GOPALA KRISHNA V <120028677+Gopala05@users.noreply.github.com>
2023-11-23 21:41:21 +0530

    WearOS

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'WearOS',
+      title: 'WEAROS',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
