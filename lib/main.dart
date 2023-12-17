4f560092dd74e5bba4d084fe03db32ece81dd400
GOPALA KRISHNA V <120028677+Gopala05@users.noreply.github.com>
2023-12-18 01:10:43 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,12 @@ Future<void> main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'WEAROS',
+      title: 'WearOS',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
